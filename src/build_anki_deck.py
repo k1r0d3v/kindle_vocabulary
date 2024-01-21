69c42db148fa530ff06461e7575cb361f2d6cbef
k1r0d3v <k1r0d3v@gmail.com>
2024-01-21 20:34:43 +0100

    Add kindle vocab.db support

--- src/build_anki_deck.py
+++ src/build_anki_deck.py
@@ -1,15 +1,17 @@
 import genanki
 import re
 from pathlib import Path
-from anki_notes import *
 import json
-from anki_notes import NoteBuilder
 import hashlib
-from build_vocabulary_index import *
 import argparse
+import base64
+
+import anki_notes
+import index_builder
+import vocabdb
 
 
-class KindleNoteBuilder(NoteBuilder):
+class KindleNoteBuilder(anki_notes.NoteBuilder):
     def __init__(self, model_name: str, front: Path, back: Path, css: Path) -> None:
         front = front.read_text(encoding='utf-8')
         back = back.read_text(encoding='utf-8')
@@ -35,8 +37,8 @@ class KindleNoteBuilder(NoteBuilder):
         ))
 
 
-class KindleDeckBuilder(DeckBuilder):
-    def __init__(self, deck_name: str, index_dir_path: Path, note_builder: NoteBuilder) -> None:
+class KindleDeckBuilder(anki_notes.DeckBuilder):
+    def __init__(self, deck_name: str, index_dir_path: Path, note_builder: anki_notes.NoteBuilder) -> None:
         deck_id=int(hashlib.shake_256(deck_name.encode(encoding='utf-8')).hexdigest(7), base=16)
         
         super().__init__(deck_id, deck_name, note_builder)
@@ -78,45 +80,82 @@ class KindleDeckBuilder(DeckBuilder):
     def __get_meanings(translation) -> str:
         meanings = ''
         for translation in translation['translations']:
-            if translation['title'] == 'Principal Translations':
-                for entry in translation['entries']:
-                    meanings += f'<span class="en gray01">{entry["from_word"]["source"]} => ({entry["context"]})</span></br>'
-                    for word in entry['to_word']:
-                        meanings += f'<span class="es cyan meaning">{word["meaning"]}</span></br>'
-                    meanings += '</br>'
+            # if translation['title'] == 'Principal Translations':
+            for entry in translation['entries']:
+                meanings += f'<span class="en gray01">{entry["from_word"]["source"]} => ({entry["context"]})</span></br>'
+                for word in entry['to_word']:
+                    meanings += f'<span class="es cyan meaning">{word["meaning"]}</span></br>'
+                meanings += '</br>'
+            
+                from_example = entry["from_example"]
+                to_example = entry["to_example"]
                 
-                    from_example = entry["from_example"]
-                    to_example = entry["to_example"]
-                    
-                    if from_example is not None:
-                        meanings += f'<span class="en ensentence gray02">{from_example}</span></br>'
-                    if to_example is not None and len(to_example) > 0:
-                        to_example = to_example[0]
-                        meanings += f'<span class="es essentence gray00">{to_example}</span></br>'
-                    meanings += '</br>'
+                if from_example is not None:
+                    meanings += f'<span class="en ensentence gray02">{from_example}</span></br>'
+                if to_example is not None and len(to_example) > 0:
+                    to_example = to_example[0]
+                    meanings += f'<span class="es essentence gray00">{to_example}</span></br>'
+                meanings += '</br>'
         return meanings
+
+
+def rmdir(directory: Path):
+    for item in directory.iterdir():
+        if item.is_dir():
+            rmdir(item)
+        else:
+            item.unlink()
+    directory.rmdir()
+
 
 if __name__ == '__main__':
     args_parser = argparse.ArgumentParser()
-    args_parser.add_argument('name', help='Anki deck name')
-    args_parser.add_argument('--input', default='vocabulary.csv', help='Vocabulary input file')
+    args_parser.add_argument('--name', help='Anki deck name, defaults to Unknown')
+    args_parser.add_argument('--input', default='vocab.db', help='Vocabulary input file (db or csv)')
+    args_parser.add_argument('--book_id', help='If the input file is a kindle vocabulary database you must select the book with this option')
     args_parser.add_argument('--index_dir', default='.vindex', help='Index directory where store the temporal generated files')
     args_parser.add_argument('--note_template_dir', default='note_template', help='Note styling directory with front.html, back.html and style.css files')
     args_parser.add_argument('--output', default='vocabulary.apkg', help='The output anki deck file name')
+    args_parser.add_argument('--to_lang', default='es', help='The language into which to translate the vocabulary')
+    args_parser.add_argument('--clear_index', action='store_true')
     args = args_parser.parse_args()
-    
-    vocabulary_file_path = Path(args.input)
-    if not vocabulary_file_path.is_file():
-        raise ValueError('Could not open vocabulary file')
     
     index_dir_path = Path(args.index_dir)
     index_dir_path.mkdir(exist_ok=True)
-    build_vocabulary_index_with_translations(vocabulary_file_path, index_dir_path)
+
+    vocabulary_file_path = Path(args.input)
+    if not vocabulary_file_path.is_file():
+        raise ValueError(f'Could not open vocabulary file "{args.input}"')  
     
+    if vocabulary_file_path.suffix == '.csv':
+        index_builder.build_vocabulary_index_with_translations_from_csv(vocabulary_file_path, index_dir_path, args.to_lang)
+    elif vocabulary_file_path.suffix == '.db':
+        with vocabdb.Vocabdb(vocabulary_file_path) as db:
+            if args.book_id is None:
+                print('No book id given, listing all available books:')
+                for book in db.get_books().values():
+                    print(f'id: {book.id}, title: {book.title}')
+                exit(0)
+            else:
+                if args.name is None:
+                    for book in db.get_books().values():
+                        if book.id == args.book_id:
+                            args.name = f'{book.title}.apkg'
+
+                index_builder.build_vocabulary_index_with_translations_from_db(args.book_id, db, index_dir_path, args.to_lang)
+    else:
+        raise ValueError('Unexpected input file type, expected types are .db or .csv')
+    
+    if args.name is None:
+        args.name = 'vocabulary.apkg'
+
     note_template_dir = Path(args.note_template_dir)
     if not note_template_dir.exists() or not note_template_dir.is_dir():
-        raise ValueError('Could not open note template directory')
+        raise ValueError(f'Could not open note template directory "{args.note_template_dir}"')
     
+    if args.clear_index:
+        rmdir(index_dir_path)
+
     note_builder = KindleNoteBuilder(
         model_name='Kindle Vocabulary Note Type',
         front=note_template_dir.joinpath('front.html'),
---
+++ src/vocabdb.py
@@ -0,0 +1,106 @@
+from argparse import Namespace
+from pathlib import Path
+import sqlite3
+
+
+class Vocabdb:
+    def __init__(self, db: Path) -> None:
+        self.__db_path = db
+        self.__db = None
+       
+    def open(self):
+        if self.__db is not None:
+            raise ValueError('Database already open')
+        
+        self.__db = sqlite3.connect(str(self.__db_path))
+
+    def close(self):
+        if self.__db is not None:
+            self.__db.close()
+            self.__db = None
+       
+    def get_books(self):
+        self.__check_db_open()
+        
+        cursor = self.__db.cursor()
+        cursor.execute(f'select * from BOOK_INFO')
+        
+        books = {}
+        for row in cursor.fetchall():
+            books[row[0]] = Namespace(**{
+                'id': row[0],
+                'asin': row[1],
+                'guid': row[2],
+                'lang': row[3],
+                'title': row[4],
+                'authors': row[5]
+            })
+        
+        cursor.close()
+
+        return books
+        
+    def get_lookups(self, book_id: str = None):
+        self.__check_db_open()
+        
+        cursor = self.__db.cursor()
+
+        query = 'select * from LOOKUPS l'
+
+        if book_id is not None:
+            query += f" where l.book_key = '{book_id}'"
+
+        cursor.execute(query)
+        
+        lookups = {}
+        for row in cursor.fetchall():
+            lookups[row[0]] = Namespace(**{
+                'id': row[0],
+                'word_id': row[1],
+                'book_id': row[2],
+                'dict_id': row[3],
+                'pos': row[4],
+                'usage': row[5],
+                'timestamp': row[6],
+            })
+        
+        cursor.close()
+
+        return lookups
+
+    def get_words(self, book_id: str = None):
+        self.__check_db_open()
+        
+        cursor = self.__db.cursor()
+        if book_id is None:
+            cursor.execute(f'select * from WORDS')
+        else:
+            cursor.execute(f"select w.id, w.word, w.stem, w.lang, w.category, w.timestamp, w.profileid from LOOKUPS l join WORDS w on l.word_key = w.id where l.book_key = '{book_id}'")
+
+        words = {}
+        for row in cursor.fetchall():
+            words[row[0]] = Namespace(**{
+                'id': row[0],
+                'value': row[1],
+                'stem': row[2],
+                'lang': row[3],
+                'category': row[4],
+                'timestamp': row[5],
+                'profileid': row[6]
+            })
+        
+        cursor.close()
+
+        return words
+
+
+    def __check_db_open(self):
+        if self.__db is None:
+            raise ValueError('Open the database before using it')
+
+    def __enter__(self):
+        self.open()
+        return self
+    
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        self.close()
